bcbe2e8be43f0cb826319cffc442e1bf9abc573b
Martekode <BrianDesmedt9000@hotmail.com>
2022-11-29 15:29:05 +0100

    packages creation works

--- server/services/PackagesService.js
+++ server/services/PackagesService.js
@@ -40,7 +40,7 @@ class PackagesService {
             adressed_name
         } = obj
 
-        const createPackageQuery = `INSERT INTO Packages(street_name,house_number,postal_code,adressed_name) VALUES('${street_name}','${house_number}','${postal_code}','${adressed_name}')`;
+        const createPackageQuery = `INSERT INTO Packages(street_name,house_number,postal_code,adressed_name) VALUES("${street_name}",${house_number},${postal_code},"${adressed_name}")`;
         return new Promise((resolve,reject) => {
             this.con.query(createPackageQuery, async function(err, result, field) {
                 if(err) throw err;
