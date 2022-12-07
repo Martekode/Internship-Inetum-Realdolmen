8a11a8de41725c18a2862a0e717b1f35f12ecb29
Martekode <BrianDesmedt9000@hotmail.com>
2022-12-07 14:59:34 +0100

    handle offline changes : posts + import iPackage

--- angular-pwa-app/src/app/app.component.ts
+++ angular-pwa-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { DataService } from './data.service';
+import { IPackage } from './package/package.component';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { DataService } from './data.service';
 export class AppComponent {
   title = 'angular-pwa-app';
   packages: any;
+  sessionPackages : Array<IPackage> = [];
   constructor(private updates: SwUpdate,private data: DataService){
     this.updates.available.subscribe(event => {
       updates.activateUpdate().then(()=> window.location.reload());
@@ -21,8 +23,35 @@ export class AppComponent {
   }
 
   ngOnInit(){
+    switch (navigator.onLine){
+      case true:
+        this.createPackagesFromSession();
+        break;
+      case false:
+        this.handleOfflineSessionPackages();
+        break;
+
+    }
     this.data.giveMeAllPackages().subscribe(res => {
       this.packages = res;
     })
+  }
+
+  createPackagesFromSession(){
+    if(sessionStorage['posts']){
+      const newEntries = JSON.parse(sessionStorage['posts']);
+      newEntries.forEach((obj : Object) => {
+        this.data.createPackage(obj);
+      });
+      sessionStorage.removeItem('posts');
+      this.sessionPackages = [];
+    }
+  }
+  handleOfflineSessionPackages(){
+    const newEntries = JSON.parse(sessionStorage['posts']);
+    newEntries.forEach((obj : IPackage) => {
+      this.sessionPackages.push(obj);
+    });
+    window.location.reload();
   }
 }
